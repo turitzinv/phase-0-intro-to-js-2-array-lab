56ac13a3fa692b6d95123e3e9670f425f9906151
Victor Turitzin <turitzinv@gmail.com>
2021-11-08 19:32:29 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    return cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(name) {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat(name) {
+    return cats.shift();
+}
+
+function appendCat(name) {
+    const catsCopy = [...cats, "Broom"];
+    return catsCopy;
+}
+
+function prependCat(name) {
+    const catsCopy = ["Arnold", ...cats];
+    return catsCopy;
+}
+
+function removeLastCat(name) {
+    const catsCopy = [...cats];
+    return catsCopy.slice(0, cats.length-1);
+}
+
+function removeFirstCat(name) {
+    const catsCopy = [...cats];
+    return catsCopy.splice(1);
+}
